8f2c8ffaec29543e7aab8cde8023c3cdaf7e2596
Luke <kodastudioai@gmail.com>
2026-07-01 19:01:07 +0300

    update l

--- lct.go
+++ lct.go
@@ -18,89 +18,112 @@ type LCTState struct {
 
 func NewLCTState(seed []byte) *LCTState {
 	h := sha256.Sum256(seed)
-	s := new(big.Int).SetUint64(binary.BigEndian.Uint64(h[:8]))
+	sVal := binary.BigEndian.Uint64(h[:8])
 	p := new(big.Int).SetUint64(LCT_P)
 	return &LCTState{
 		P:     p,
-		S:     new(big.Int).Mod(s, p),
+		S:     new(big.Int).Mod(new(big.Int).SetUint64(sVal), p),
 		index: 0,
 	}
 }
 
 func (st *LCTState) Evolve(block []byte) {
-	term1 := new(big.Int).Exp(st.S, big.NewInt(3), st.P)
-	term2 := new(big.Int).Mul(big.NewInt(13), st.S)
-	term2.Mod(term2, st.P)
+	p := st.P
+	s := st.S
 
+	// Si^3 mod P
+	term1 := new(big.Int).Exp(s, big.NewInt(3), p)
+
+	// 13Si mod P
+	term2 := new(big.Int).Mul(big.NewInt(13), s)
+	term2.Mod(term2, p)
+
+	// 7i^2 mod P
 	idx := big.NewInt(int64(st.index))
 	term3 := new(big.Int).Mul(big.NewInt(7), new(big.Int).Mul(idx, idx))
-	term3.Mod(term3, st.P)
+	term3.Mod(term3, p)
 
+	// Sum(k+1)bk mod P
 	sum := big.NewInt(0)
 	for k, b := range block {
 		bk := big.NewInt(int64(b))
 		coeff := big.NewInt(int64(k + 1))
 		sum.Add(sum, new(big.Int).Mul(coeff, bk))
 	}
-	sum.Mod(sum, st.P)
+	sum.Mod(sum, p)
 
-	st.S.Add(term1, term2)
-	st.S.Add(st.S, term3)
-	st.S.Add(st.S, sum)
-	st.S.Mod(st.S, st.P)
+	// S(i+1) = (term1 + term2 + term3 + sum) mod P
+	newS := new(big.Int).Add(term1, term2)
+	newS.Add(newS, term3)
+	newS.Add(newS, sum)
+	newS.Mod(newS, p)
+
+	st.S = newS
 	st.index++
 }
 
 func (st *LCTState) Project(b byte) (x, y, z *big.Int) {
-	x = new(big.Int).Add(new(big.Int).SetUint64(uint64(b)), st.S)
-	x.Mod(x, st.P)
+	p := st.P
+	s := st.S
+
+	x = new(big.Int).Add(new(big.Int).SetUint64(uint64(b)), s)
+	x.Mod(x, p)
 
 	y = new(big.Int).Mul(x, x)
-	y.Add(y, new(big.Int).Mul(big.NewInt(5), st.S))
-	y.Mod(y, st.P)
+	y.Add(y, new(big.Int).Mul(big.NewInt(5), s))
+	y.Mod(y, p)
 
 	z = new(big.Int).Mul(big.NewInt(3), x)
 	z.Add(z, new(big.Int).Mul(big.NewInt(7), y))
-	z.Add(z, st.S)
-	z.Mod(z, st.P)
+	z.Add(z, s)
+	z.Mod(z, p)
 	return
 }
 
 func (st *LCTState) Mix(x, y, z *big.Int) (v1, v2, v3 *big.Int) {
-	// M = [ 2+s, 5, 7 ]
-	//     [ 11, 3+s, 13 ]
-	//     [ 17, 19, 23+s ]
-	a := new(big.Int).Add(big.NewInt(2), st.S)
-	e := new(big.Int).Add(big.NewInt(3), st.S)
-	i := new(big.Int).Add(big.NewInt(23), st.S)
+	p := st.P
+	s := st.S
+
+	a := new(big.Int).Add(big.NewInt(2), s)
+	b := big.NewInt(5)
+	c := big.NewInt(7)
+	d := big.NewInt(11)
+	e := new(big.Int).Add(big.NewInt(3), s)
+	f := big.NewInt(13)
+	g := big.NewInt(17)
+	h := big.NewInt(19)
+	i := new(big.Int).Add(big.NewInt(23), s)
 
 	v1 = new(big.Int).Mul(a, x)
-	v1.Add(v1, new(big.Int).Mul(big.NewInt(5), y))
-	v1.Add(v1, new(big.Int).Mul(big.NewInt(7), z))
-	v1.Mod(v1, st.P)
+	v1.Add(v1, new(big.Int).Mul(b, y))
+	v1.Add(v1, new(big.Int).Mul(c, z))
+	v1.Mod(v1, p)
 
-	v2 = new(big.Int).Mul(big.NewInt(11), x)
+	v2 = new(big.Int).Mul(d, x)
 	v2.Add(v2, new(big.Int).Mul(e, y))
-	v2.Add(v2, new(big.Int).Mul(big.NewInt(13), z))
-	v2.Mod(v2, st.P)
+	v2.Add(v2, new(big.Int).Mul(f, z))
+	v2.Mod(v2, p)
 
-	v3 = new(big.Int).Mul(big.NewInt(17), x)
-	v3.Add(v3, new(big.Int).Mul(big.NewInt(19), y))
+	v3 = new(big.Int).Mul(g, x)
+	v3.Add(v3, new(big.Int).Mul(h, y))
 	v3.Add(v3, new(big.Int).Mul(i, z))
-	v3.Mod(v3, st.P)
+	v3.Mod(v3, p)
 	return
 }
 
 func (st *LCTState) Unmix(v1, v2, v3 *big.Int) (x, y, z *big.Int) {
-	a := new(big.Int).Add(big.NewInt(2), st.S)
+	p := st.P
+	s := st.S
+
+	a := new(big.Int).Add(big.NewInt(2), s)
 	b := big.NewInt(5)
 	c := big.NewInt(7)
 	d := big.NewInt(11)
-	e := new(big.Int).Add(big.NewInt(3), st.S)
+	e := new(big.Int).Add(big.NewInt(3), s)
 	f := big.NewInt(13)
 	g := big.NewInt(17)
 	h := big.NewInt(19)
-	i := new(big.Int).Add(big.NewInt(23), st.S)
+	i := new(big.Int).Add(big.NewInt(23), s)
 
 	// Cofactors
 	C11 := new(big.Int).Sub(new(big.Int).Mul(e, i), new(big.Int).Mul(f, h))
@@ -111,9 +134,9 @@ func (st *LCTState) Unmix(v1, v2, v3 *big.Int) (x, y, z *big.Int) {
 	det := new(big.Int).Mul(a, C11)
 	det.Add(det, new(big.Int).Mul(b, C12))
 	det.Add(det, new(big.Int).Mul(c, C13))
-	det.Mod(det, st.P)
+	det.Mod(det, p)
 
-	detInv := new(big.Int).ModInverse(det, st.P)
+	detInv := new(big.Int).ModInverse(det, p)
 
 	C21 := new(big.Int).Sub(new(big.Int).Mul(b, i), new(big.Int).Mul(c, h))
 	C21.Neg(C21)
@@ -126,25 +149,23 @@ func (st *LCTState) Unmix(v1, v2, v3 *big.Int) (x, y, z *big.Int) {
 	C32.Neg(C32)
 	C33 := new(big.Int).Sub(new(big.Int).Mul(a, e), new(big.Int).Mul(b, d))
 
-	// [x,y,z] = detInv * adj(M) * [v1,v2,v3]
-	// adj(M) = transpose of cofactor matrix
 	x = new(big.Int).Mul(C11, v1)
 	x.Add(x, new(big.Int).Mul(C21, v2))
 	x.Add(x, new(big.Int).Mul(C31, v3))
 	x.Mul(x, detInv)
-	x.Mod(x, st.P)
+	x.Mod(x, p)
 
 	y = new(big.Int).Mul(C12, v1)
 	y.Add(y, new(big.Int).Mul(C22, v2))
 	y.Add(y, new(big.Int).Mul(C32, v3))
 	y.Mul(y, detInv)
-	y.Mod(y, st.P)
+	y.Mod(y, p)
 
 	z = new(big.Int).Mul(C13, v1)
 	z.Add(z, new(big.Int).Mul(C23, v2))
 	z.Add(z, new(big.Int).Mul(C33, v3))
 	z.Mul(z, detInv)
-	z.Mod(z, st.P)
+	z.Mod(z, p)
 	return
 }
 
@@ -171,7 +192,9 @@ func LCTDecode(vectors []uint64, seed []byte) []byte {
 		x, _, _ := st.Unmix(v1, v2, v3)
 
 		// b = (x - s) mod P
+		// Using (x - s + P) mod P to ensure positive result
 		b := new(big.Int).Sub(x, st.S)
+		b.Add(b, st.P)
 		b.Mod(b, st.P)
 
 		val := byte(b.Uint64() & 0xFF)
